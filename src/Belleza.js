e3f42359f81da81a05354a8f61b50c709739790d
enigma <camilo.murcia.pimentel@gmail.com>
2021-12-23 20:23:03 -0500

    Refactor Belleza component adding mobile version

--- src/Belleza.js
+++ src/Belleza.js
@@ -1,40 +1,83 @@
 import Container from'@mui/material/Container';
 import Box from'@mui/material/Box';
 import Typography from'@mui/material/Typography';
-import Divider from'@mui/material/Divider';
 import Button from'@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import { Title } from './Categories';
+
+
+
+const Image = styled(Box)(({theme}) => ({
+    width:"200px", 
+    height:"200px",
+    transition: "all 0.5s ease-out",
+    opacity: 0.8,
+    margin: "0 10px", 
+    [theme.breakpoints.down('md')]:{
+        width:"90vw"    
+    },
+    "&:hover": {opacity: 1, cursor: "pointer"},
+}));
+
+const Area = styled(Box)(({theme}) => ({
+    width:500, 
+    height:300, 
+    opacity:0.8,
+    transition: "all 0.5s ease-out",
+    [theme.breakpoints.down('md')]:{
+        width: "100%",
+        height: "250px",
+    },
+    "&:hover": {opacity: 1, cursor: "pointer"}
+}));
+
+const Btn = styled(Button)(({theme}) => ({
+    width: "100%",
+    borderColor:"#4f4d4d", 
+    color:"#4f4d4d",
+    margin:"10px",
+    [theme.breakpoints.down('md')]:{
+        width: "70%",
+    },
+    "&:hover": {
+        background: "#f7f5f5",
+        borderColor: "#b8b2b2"
+    },
+    
+}));
+
+const Section = styled(Box)(({theme}) => ({
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "5px",
+}))
+
+const Strong = styled(Typography)({
+    color:"#212529", 
+    fontFamily:'Lato, sans-serif', 
+    textAlign:"center"
+});
+
+
 
 const Belleza = () => {
-
+    
+    const images = [
+        {id: 1, imagePath:"mascara", text:"Cuidado facial"},
+        {id: 2, imagePath:"corporal", text:"Cuidado corporal"},
+        {id: 3, imagePath:"polvos", text:"Proteccion solar"},
+    ];
 
     return (
-        <Container sx={{marginTop: "100px"}}>
-            <Box sx={{display:"flex"}}>
-                <Box
+        <Box sx={{marginTop: "80px"}}>
+            <Box sx={{display:"flex", flexWrap:"wrap"}}>
+                <Area
                     alt="woman"
                     component="img"
                     src={require('./images/cremas2.jpg')}
-                    sx={{
-                        width:500, 
-                        height:300, 
-                        opacity:0.8,
-                        transition: "all 0.5s ease-out", 
-                        "&:hover": {opacity: 1, cursor: "pointer"}
-                    }}
                 />
                 <Box>
-                    <Typography 
-                        sx={{
-                            color:"#212529", 
-                            fontSize: "1.625rem", 
-                            fontFamily:'Lato, sans-serif',
-                            fontWeight: 400,
-                            textAlign: "center",
-                            marginBottom: "20px",
-                        }}
-                    >
-                        Belleza
-                    </Typography>
+                    <Title sx={{textAlign: "center"}}>Belleza</Title>
                     <Typography
                         sx={{
                             marginX: "50px",
@@ -46,114 +89,29 @@ const Belleza = () => {
                         Encuentra tus productos favoritos para expresar lo mejor de tu belleza. 
                         Celebra de una y mil maneras con esta selección única.
                     </Typography>
-                    <Box sx={{display: "flex", justifyContent:"center", marginTop: "20px"}}>
-                        <Button
-                            variant="outlined"
-                            sx={{
-                                margin: "0 auto", 
-                                borderColor:"#4f4d4d", 
-                                color:"#4f4d4d",
-                                "&:hover": {
-                                    background: "#f7f5f5",
-                                    borderColor: "#b8b2b2"
-                                }
-                            }}
-                        >
-                            Ver todo
-                        </Button>
+                    <Section>
+                        <Btn variant="outlined">Ver todo</Btn>
+                    </Section>
+                </Box>
+            </Box>
+
+            {/* OK */}
+            <Box sx={{display: "flex", justifyContent:"space-evenly", marginTop: 3, flexWrap:"wrap"}}>
+                {images.map(image => 
+                    <Box key={image.id} sx={{margin: "10px"}}>
+                        <Image
+                            alt="woman"
+                            component="img"
+                            src={require(`./images/${image.imagePath}.jpg`)}
+                        />
+                        <Strong>{image.text}</Strong>
+                        <Section>
+                            <Btn variant="outlined">Ver todo</Btn>
+                        </Section>
                     </Box>
-                </Box>
+                )}
             </Box>
-            <Box sx={{display: "flex", justifyContent:"space-evenly", marginTop: 3}}>
-                <Box>
-                    <Box
-                        alt="woman"
-                        component="img"
-                        src={require('./images/mascara.jpg')}
-                        sx={{
-                            width:200, 
-                            height:200,
-                            opacity: 0.8,
-                            transition: "all 0.5s ease-out", 
-                            "&:hover": {opacity: 1, cursor: "pointer", transition:""}
-                        }}
-                    />
-                    <Typography sx={{color:"#212529", fontFamily:'Lato, sans-serif', textAlign:"center"}}>Cuidado facial</Typography>
-                    <Button
-                        variant="outlined" 
-                        sx={{
-                            width: "100%",
-                            margin: "0 auto",
-                            borderColor:"#4f4d4d", 
-                            color:"#4f4d4d",
-                            marginTop:"10px",
-                            "&:hover": {
-                                background: "#f7f5f5",
-                                borderColor: "#b8b2b2"
-                            }
-                        }}
-                    >Ver todo</Button>
-                </Box>
-                <Box>
-                    <Box
-                        alt="woman"
-                        component="img"
-                        src={require('./images/corporal.jpg')}
-                        sx={{
-                            width:200, 
-                            height:200,
-                            opacity: 0.8,
-                            transition: "all 0.5s ease-out", 
-                            "&:hover": {opacity: 1, cursor: "pointer"}
-                        }}
-                    />
-                    <Typography sx={{color:"#212529", fontFamily:'Lato, sans-serif', textAlign:"center", }}>Cuidado corporal</Typography>
-                    <Button
-                        variant="outlined"
-                        sx={{
-                            width: "100%",
-                            margin: "0 auto",
-                            borderColor:"#4f4d4d", 
-                            color:"#4f4d4d",
-                            marginTop:"10px",
-                            "&:hover": {
-                                background: "#f7f5f5",
-                                borderColor: "#b8b2b2"
-                            }
-                        }}
-                    >Ver todo</Button>
-                </Box>
-                <Box>
-                    <Box
-                        alt="woman"
-                        component="img"
-                        src={require('./images/polvos.jpg')}
-                        sx={{
-                            width:200, 
-                            height:200,
-                            transition: "all 0.5s ease-out",
-                            opacity: 0.8, 
-                            "&:hover": {opacity: 1, cursor: "pointer"}
-                        }}
-                    />
-                    <Typography sx={{color:"#212529", fontFamily:'Lato, sans-serif', textAlign:"center"}}>Proteccion solar</Typography>
-                    <Button
-                        variant="outlined"
-                        sx={{
-                            width: "100%",
-                            margin: "0 auto",
-                            borderColor:"#4f4d4d", 
-                            color:"#4f4d4d",
-                            marginTop:"10px",
-                            "&:hover": {
-                                background: "#f7f5f5",
-                                borderColor: "#b8b2b2"
-                            }
-                        }}
-                    >Ver todo</Button>
-                </Box>
-            </Box>
-        </Container>
+        </Box>
     );
 };
 

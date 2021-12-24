364a5753ae4128d80effb95d82d0a5fe2e8f0fd7
enigma <camilo.murcia.pimentel@gmail.com>
2021-12-23 20:21:16 -0500

    Refactor secong higher point App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Header from "./Header";
 import Categories from "./Categories";
 import Moda from "./Moda";
 import Electro from "./Electro";
 import Belleza from "./Belleza";
 import Legend from "./Legend";
+import { styled } from '@mui/material/styles';
+import Container from "@mui/material/Container";
+
+const Div = styled(Box)(({theme}) => ({
+	marginX: "130px",
+	[theme.breakpoints.down('md')]:{
+		margin: "0 auto"
+	}
+}));
 
 const App = () => {
-
 	return (
   		<>
 		  	<Legend />
 			<Header />
-			<Categories />
-			<Box sx={{marginX: "130px"}}>
-				<Moda />
-				<Electro />
-				<Belleza />
-			</Box>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fill-opacity="0.1" d="M0,32L60,58.7C120,85,240,139,360,165.3C480,192,600,192,720,170.7C840,149,960,107,1080,106.7C1200,107,1320,149,1380,170.7L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+			<Container>
+				<Categories />
+				<Div>
+					<Moda />
+					<Electro />
+					<Belleza />
+				</Div>
+			</Container>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fillOpacity="0.1" d="M0,32L60,58.7C120,85,240,139,360,165.3C480,192,600,192,720,170.7C840,149,960,107,1080,106.7C1200,107,1320,149,1380,170.7L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
 		</>
   	);
 }
